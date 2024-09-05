7d519eb227252f92df4503b71fdda16197540839
DIEME <37548785+diemenaby@users.noreply.github.com>
2024-09-05 17:01:30 +0000

    Update Projet_Trois.py

--- Troisieme_Projet/Projet_Trois.py
+++ Troisieme_Projet/Projet_Trois.py
@@ -10,20 +10,27 @@ print(listes_totales)
 print("")
 print("Fin")
 print("---------------------------------")
-var = ("\n"
-       "Question 2\n"
-       "\n"
-       "Écrivez un programme Python pour obtenir une liste, triée par ordre croissant par le dernier élément de "
-       "chaque tuple, à partir d'une liste donnée de tuples non vides.\n"
-       "\n"
-       "Liste d'échantillons : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]\n"
-       "\n"
-       "Résultat attendu : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]\n"
-       "\n"
-       "Astuce : vous pouvez utiliser la fonction de tri.\n")
+"""
+       Question 2
+       
+       Écrivez un programme Python pour obtenir une liste, triée par ordre croissant par le dernier élément de 
+       chaque tuple, à partir d'une liste donnée de tuples non vides.
+       
+       Liste d'échantillons : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+       
+       Résultat attendu : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+       
+       Astuce : vous pouvez utiliser la fonction de tri.
+       
+"""
 print("Résolution de la question 2")
+
 listes_tries = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-print(sorted(listes_tries, reverse=False))
+
+listes_tries.sort(key=lambda trie: trie[1])
+for value in listes_tries:
+    print([value], end=", ")
+
 print("")
 print("Fin")
 print("---------------------------------")
@@ -39,15 +46,25 @@ d2 = {'a' : 300, 'b' : 200, 'd' : 400}
 Résultat attendu : {'a' : 400, 'b' : 400, 'd' : 400, 'c' : 300}
 """
 print("Résolution de la question 3")
+import collections
+
 d1 = {'a': 100, 'b': 200, 'c': 300}
 
 d2 = {'a': 300, 'b': 200, 'd': 400}
-d3 = d1 | d2
-print(d3)
+d3 = [d1, d2]
+compter = collections.Counter()
+for d in d3:
+    compter.update(d)
+
+result = dict(compter)
+print()
+print("Résultat attendu : ", result)
+
 print()
 print("Fin")
 print("---------------------------------")
 """
+Question 4
 Avec un nombre entier n donné, écrivez un programme pour générer un dictionnaire 
 qui contient (i, i*i) de sorte que soit un nombre entier compris entre 1 et 
 n (les deux inclus). Le programme doit ensuite imprimer le dictionnaire. 
@@ -58,7 +75,7 @@ print()
 print("Résolution de la question 4")
 Entrer_Nombre = int(input("Veiller saisir un entier s'il vous plait :"))
 Entrer_Nombre = Entrer_Nombre + 1
-Nombre_Saisi = {Nombre: Nombre ** 2 for Nombre in range(Entrer_Nombre)}
+Nombre_Saisi = {Nombre: Nombre ** 2 for Nombre in range(1, Entrer_Nombre)}
 print(Nombre_Saisi)
 print()
 print("Fin")
@@ -91,15 +108,16 @@ Exemples : {0, 1, 2, 3, 4}
 Écrivez un programme Python pour effectuer une itération sur des ensembles.
 
 Écrivez un programme Python pour ajouter des membres à un ensemble et pour supprimer des éléments d'un ensemble donné.
-"""
 
+"""
 print()
 print("Résolution de la question 6")
 ensemble = {0, 1, 2, 3, 4}
 for value in ensemble:
-    print(value)
+    print(value, end=", ")
 ajout = int(input("Veiller saisir un entier s'il vous plait :"))
 ensemble.add(ajout)
+print(ensemble)
 supprimer = int(input("Veiller supprimer un entier s'il vous plait :"))
 ensemble.remove(supprimer)
 print(ensemble)
